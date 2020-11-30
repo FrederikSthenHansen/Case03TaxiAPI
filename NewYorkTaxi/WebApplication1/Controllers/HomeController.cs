c710ce2287ecb4da9b59ef8aa160724f4d40ad1b
Frederik Sthen Hansen <38318697+FrederikSthenHansen@users.noreply.github.com>
2020-11-30 15:33:52 +0100

    New MVC project added for quick test

---
+++ NewYorkTaxi/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+using NewYorkTaxi.DisplayModel;
+using System.IO;
+using Newtonsoft.Json;
+using NewYorkTaxi;
+using System.Net.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private AveragePassengerDisplay Display;
+        public static List<NewYorkTaxi.AvgPassenger> items;
+        //Husk at lave en et par ekstra kodelinjer til at have en ny hardcoded filepath til Json filen, hvis jeg koder hjemmefra
+        readonly string myJsonFilePath = @"C:\Users\SA02- Frederik\Documents\Case03TaxiAPI\Case03TaxiAPI\NewYorkTaxi\NewYorkTaxi\Files\QueryResult.Json";
+
+        public HomeController(/*MvcDbContext context*/)
+        {
+            //_context = context;
+            Display = new AveragePassengerDisplay();
+
+        }
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async IActionResult Privacy()
+        {
+
+
+            string SoQL = "https://data.cityofnewyork.us/resource/t29m-gskq.json?$select=vendorid,passenger_count";
+
+            // write JSON directly to a file
+            System.IO.StreamWriter QueryWriter = new StreamWriter(myJsonFilePath);
+
+            //Create a new instance of HttpClient
+            using (HttpClient client = new HttpClient())
+            {
+                //Setting up the response...         
+
+                using (HttpResponseMessage res = await client.GetAsync(SoQL))
+                using (HttpContent content = res.Content)
+                {
+                    string data = await content.ReadAsStringAsync();
+                    if (data != null)
+                    {
+                        QueryWriter.Write(data);
+                    }
+                }
+            }
+
+            //open and deserialize result
+            using (StreamReader reader = new StreamReader(myJsonFilePath))
+            {
+                string json = reader.ReadToEnd();
+
+                //denne Deserializering skal sikkert finjusteres.
+                items = JsonConvert.DeserializeObject<List<AvgPassenger>>(json);
+
+            }
+
+            items.GroupBy(x => x.VendorID);
+
+            Display.AvgPassengers = items;
+            //Mangler at finde en måde at få mit view frem som resultat af Requested
+            return View(Display);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
