6c3a4888cf44ec53d8bd12515a29a4be0e42279a
Frederik Sthen Hansen <38318697+FrederikSthenHansen@users.noreply.github.com>
2020-11-30 15:59:47 +0100

    To do markeret

--- NewYorkTaxi/WebApplication1/Controllers/HomeController.cs
+++ NewYorkTaxi/WebApplication1/Controllers/HomeController.cs
@@ -22,33 +22,35 @@ namespace WebApplication1.Controllers
         //Husk at lave en et par ekstra kodelinjer til at have en ny hardcoded filepath til Json filen, hvis jeg koder hjemmefra
         readonly string myJsonFilePath = @"C:\Users\SA02- Frederik\Documents\Case03TaxiAPI\Case03TaxiAPI\NewYorkTaxi\NewYorkTaxi\Files\QueryResult.Json";
 
-        public HomeController(/*MvcDbContext context*/)
-        {
+        public HomeController(/*MvcDbContext context*/ILogger<HomeController> logger)
+        {_logger = logger;
             //_context = context;
             Display = new AveragePassengerDisplay();
 
         }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+       
 
         public IActionResult Index()
         {
+            
             return View();
         }
 
-        public async IActionResult Privacy()
+        public async Task<IActionResult> Privacy()
         {
-
-
+            myJsonFilePath
             string SoQL = "https://data.cityofnewyork.us/resource/t29m-gskq.json?$select=vendorid,passenger_count";
 
             // write JSON directly to a file
             System.IO.StreamWriter QueryWriter = new StreamWriter(myJsonFilePath);
 
-            //Create a new instance of HttpClient
+            //TODO: Clear JSonfile at the beginning of content.
+            
+
+            //Create a new instance of HttpClient, and make the HTTP request and write the response to a JSon file
+
+            //Start of section needed to be cut out and placed in seperate method
             using (HttpClient client = new HttpClient())
             {
                 //Setting up the response...         
@@ -64,6 +66,9 @@ namespace WebApplication1.Controllers
                 }
             }
 
+            //End of section needed to be cut out and placed in seperat Method
+
+
             //open and deserialize result
             using (StreamReader reader = new StreamReader(myJsonFilePath))
             {
@@ -74,12 +79,15 @@ namespace WebApplication1.Controllers
 
             }
 
-            items.GroupBy(x => x.VendorID);
+            //Seperate the vendors
+            //items.GroupBy(x => x.VendorID);
 
             Display.AvgPassengers = items;
             //Mangler at finde en måde at få mit view frem som resultat af Requested
             return View(Display);
         }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
