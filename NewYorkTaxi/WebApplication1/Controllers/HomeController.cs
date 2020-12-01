e3eb094f1fb49cfb3156bb508a2dac6e713fc618
Frederik Sthen Hansen <38318697+FrederikSthenHansen@users.noreply.github.com>
2020-12-01 12:15:40 +0100

    First query to view works 80%

--- NewYorkTaxi/WebApplication1/Controllers/HomeController.cs
+++ NewYorkTaxi/WebApplication1/Controllers/HomeController.cs
@@ -16,16 +16,19 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private myJsonreader myReader;
         private readonly ILogger<HomeController> _logger;
         private AveragePassengerDisplay Display;
         public static List<NewYorkTaxi.AvgPassenger> items;
         //Husk at lave en et par ekstra kodelinjer til at have en ny hardcoded filepath til Json filen, hvis jeg koder hjemmefra
         readonly string myJsonFilePath = @"C:\Users\SA02- Frederik\Documents\Case03TaxiAPI\Case03TaxiAPI\NewYorkTaxi\NewYorkTaxi\Files\QueryResult.Json";
 
+        JsonSerializerSettings mySettings = new JsonSerializerSettings();
         public HomeController(/*MvcDbContext context*/ILogger<HomeController> logger)
         {_logger = logger;
             //_context = context;
             Display = new AveragePassengerDisplay();
+            
 
         }
 
@@ -37,25 +40,20 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public async Task<IActionResult> Privacy()
+
+        private async Task<bool> writeJsonResponse(string myUrl)
         {
-            myJsonFilePath
-            string SoQL = "https://data.cityofnewyork.us/resource/t29m-gskq.json?$select=vendorid,passenger_count";
 
             // write JSON directly to a file
             System.IO.StreamWriter QueryWriter = new StreamWriter(myJsonFilePath);
 
-            //TODO: Clear JSonfile at the beginning of content.
-            
 
             //Create a new instance of HttpClient, and make the HTTP request and write the response to a JSon file
-
-            //Start of section needed to be cut out and placed in seperate method
             using (HttpClient client = new HttpClient())
             {
                 //Setting up the response...         
 
-                using (HttpResponseMessage res = await client.GetAsync(SoQL))
+                using (HttpResponseMessage res = await client.GetAsync(myUrl))
                 using (HttpContent content = res.Content)
                 {
                     string data = await content.ReadAsStringAsync();
@@ -65,30 +63,51 @@ namespace WebApplication1.Controllers
                     }
                 }
             }
+            QueryWriter.Close();
+            return true;
+        }
 
-            //End of section needed to be cut out and placed in seperat Method
+        public async Task<IActionResult> Privacy()
+        {
+            //clean up jsonfile from previous query
+            System.IO.File.Delete(myJsonFilePath);
 
+            string SoQL = "https://data.cityofnewyork.us/resource/t29m-gskq.json?$select=vendorid,passenger_count";
 
-            //open and deserialize result
-            using (StreamReader reader = new StreamReader(myJsonFilePath))
+            //send Query and write response to Jsonfile
+           
+
+            if (await writeJsonResponse(SoQL)== true)
             {
-                string json = reader.ReadToEnd();
+                //open Jsonfile and deserialize content
+                using (StreamReader reader = new StreamReader(myJsonFilePath))
+                {
+                    string json = reader.ReadToEnd();
 
-                //denne Deserializering skal sikkert finjusteres.
-                items = JsonConvert.DeserializeObject<List<AvgPassenger>>(json);
+                    //denne Deserializering skal sikkert finjusteres. mine settins skal sættes rigtigt op.
+                    items = JsonConvert.DeserializeObject<List<AvgPassenger>>(json,mySettings);
 
+                }
+
+                //Seperate the vendors
+                //items.GroupBy(x => x.VendorID);
+
+                
+
+                Display.AvgPassengers = items;
             }
-
-            //Seperate the vendors
-            //items.GroupBy(x => x.VendorID);
-
-            Display.AvgPassengers = items;
             //Mangler at finde en måde at få mit view frem som resultat af Requested
             return View(Display);
         }
 
 
+        //public void readJson()
+        //{
+        //    myReader.SetToken(myJsonFilePath)
+        //    myReader.Read();
 
+
+        //}
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ NewYorkTaxi/WebApplication1/Models/myJsonreader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class myJsonreader : Newtonsoft.Json.JsonReader
+    {
+       
+        public override bool Read()
+        {
+            return true;
+        }
+    }
+}
