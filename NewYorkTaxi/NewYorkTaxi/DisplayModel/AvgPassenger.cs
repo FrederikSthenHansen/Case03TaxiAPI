20de0f6666148fa93ee712ee7eaef7b2d97e5318
Frederik Sthen Hansen <38318697+FrederikSthenHansen@users.noreply.github.com>
2020-12-03 15:11:41 +0100

    home controller cleaned up

--- NewYorkTaxi/NewYorkTaxi/DisplayModel/AvgPassenger.cs
+++ NewYorkTaxi/NewYorkTaxi/DisplayModel/AvgPassenger.cs
@@ -3,28 +3,22 @@ using System;
 namespace NewYorkTaxi
 {
     /// <summary>
-    /// Class to hold a vendor iD and an int for Passenger count. name is misleading. 
+    /// Class to store Any kind of Taxi data. Name is misleading 
     /// </summary>
     public class AvgPassenger
     {
         public int VendorID { get; set; }
 
-        
-        
         public int Passenger_Count { get; set; }
+
+        public decimal Total_Amount { get; set; }
+
+        public decimal Trip_Distance { get; set; }
 
         public AvgPassenger()
         {
 
         }
-        public AvgPassenger(String mock)
-        {
-            if (mock == "mock")
-            {
-                Random rng = new Random();
-                VendorID = rng.Next(1, 3);
-                Passenger_Count = rng.Next(0, 6);
-            }
-        }
+        
     }
 }
--- NewYorkTaxi/WebApplication1/Controllers/HomeController.cs
+++ NewYorkTaxi/WebApplication1/Controllers/HomeController.cs
@@ -17,30 +17,23 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-       
         private readonly ILogger<HomeController> _logger;
         private AveragePassengerDisplay Display;
         public static List<NewYorkTaxi.AvgPassenger> items;
+
         //Husk at lave en et par ekstra kodelinjer til at have en ny hardcoded filepath til Json filen, hvis jeg koder hjemmefra
         readonly string myJsonFilePath = @"C:\Users\SA02- Frederik\Documents\Case03TaxiAPI\Case03TaxiAPI\NewYorkTaxi\NewYorkTaxi\Files\QueryResult.Json";
 
-        JsonSerializerSettings mySettings = new JsonSerializerSettings();
-        public HomeController(/*MvcDbContext context*/ILogger<HomeController> logger)
-        {_logger = logger;
-            //_context = context;
+        public HomeController(ILogger<HomeController> logger)
+        {   
+            _logger = logger;
             Display = new AveragePassengerDisplay();
-            
-
         }
-
-       
 
         public IActionResult Index()
-        {
-            
+        { 
             return View();
         }
-
 
         private async Task<bool> writeJsonResponse(string myUrl)
         {
@@ -77,18 +70,14 @@ namespace WebApplication1.Controllers
             string SoQL = "https://data.cityofnewyork.us/resource/t29m-gskq.json?$select=vendorid,passenger_count";
 
             //send Query and write response to Jsonfile
-
-
             if (await writeJsonResponse(SoQL)== true)
             {
                 JObject obj = null;
                 JsonSerializer jsonSerializer = new JsonSerializer();
-                //open Jsonfile and deserialize content
 
+                //open Jsonfile and deserialize content
                 if (System.IO.File.Exists(myJsonFilePath))
                 {
-                    
-
                     using (StreamReader reader = new StreamReader(myJsonFilePath))
                     {
                        string json= reader.ReadToEnd();
@@ -97,49 +86,19 @@ namespace WebApplication1.Controllers
 
                         //Remember to Always mirror the Database property names in your c# object properies!!!
                         items = JsonConvert.DeserializeObject<List<AvgPassenger>>(json);
-
-                      
                     }
                 }
 
-                //placeholder til frontend
-                //items.Clear();
-                //for (int x = 0; x < 40; x++)
-                //{
-                //    items.Add(new AvgPassenger("mock"));
-                //}
-                //ende på placeholder
-
-
-  
                //Deliver items to DisplayModel
                 Display.AvgPassengers = items;
 
                 //Seperate the vendors in the displaymodel
                 Display.SortDistinctVendors();
             }
-            //Mangler at finde en måde at få mit view frem som resultat af Requested
+            
             return View(Display);
         }
 
-        private List<AvgPassenger> ReadAvgPassenger(StreamReader readerParam)
-        {
-            List<AvgPassenger> ret=new List<AvgPassenger>();
-            AvgPassenger obj= new AvgPassenger();
-            obj.VendorID = readerParam.Read(); //find a way to read each property.
-
-
-            ret.Add(obj);
-            return ret;
-        }
-
-        //public void readJson()
-        //{
-        //    myReader.SetToken(myJsonFilePath)
-        //    myReader.Read();
-
-
-        //}
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
